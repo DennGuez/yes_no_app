3667a375c8ec4e4acfdd5979b6ce0bf8fa6f14b9
Dennis Rodriguez <dennisrodriguezx@gmail.com>
2023-05-04 22:46:36 -0400

    FEAT: http req added, and Mappers Manually

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 /* ChangeNotifier nos dice basicamente esta clase puede notificar */
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScrollController = ScrollController();
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messageList = [
     Message(text: 'Hola my love', fromWho: FromWho.me),
@@ -15,6 +17,18 @@ class ChatProvider extends ChangeNotifier {
 
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
+
+    if (text.endsWith('?')) {
+      herReply();
+    }
+
+    notifyListeners(); // Notificamos a todos lo que estan escuchando que algo del provider cambio
+    moveScrollToBottom(); // Hacemos que los ultimos mensajes siempre se vean scrolleando hacia abajo
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+    messageList.add(herMessage);
     notifyListeners(); // Notificamos a todos lo que estan escuchando que algo del provider cambio
     moveScrollToBottom(); // Hacemos que los ultimos mensajes siempre se vean scrolleando hacia abajo
   }
@@ -26,14 +40,13 @@ class ChatProvider extends ChangeNotifier {
   //       curve: Curves.easeOut);
   // }
 
- /* Para que la animacion tarde un poco mas */
+  /* Para que la animacion tarde un poco mas */
   Future<void> moveScrollToBottom() async {
-    await Future.delayed(const Duration(microseconds: 2000));
+    await Future.delayed(const Duration(microseconds: 100));
 
     chatScrollController.animateTo(
-      chatScrollController.position.maxScrollExtent,
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeOut
-    );
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut);
   }
 }
